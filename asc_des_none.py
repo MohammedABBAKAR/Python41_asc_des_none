6cf4ff6ae5b346e6d94f04a44ce7cccb9782ebb8
Mohammed ABBAKAR <73604525+MohammedABBAKAR@users.noreply.github.com>
2024-08-28 22:34:13 +0200

    Let's Sort This List!

---
+++ asc_des_none.py
@@ -0,0 +1,40 @@
+# Let's Sort This List!
+# Create a function that takes a list of numbers lst, a string s and return a list of numbers as per the following rules:
+
+# "Asc" returns a sorted list in ascending order.
+# "Des" returns a sorted list in descending order.
+# "None" returns a list without any modification.
+# Examples
+# asc_des_none([4, 3, 2, 1], "Asc" ) ➞ [1, 2, 3, 4]
+
+# asc_des_none([7, 8, 11, 66], "Des") ➞ [66, 11, 8, 7]
+
+# asc_des_none([1, 2, 3, 4], "None") ➞ [1, 2, 3, 4]
+# Notes
+
+
+
+
+
+def asc_des_none(lst, s):
+    if s == "Asc":
+        return sorted(lst)
+    elif s == "Des":
+        return sorted(lst,reverse=True)
+    elif s == "None":
+        return lst
+print(asc_des_none([4, 3, 2, 1], "None" ))
+
+
+
+
+
+def asc_des_none(lst, s):
+    if s == "Asc":
+        return sorted(lst)
+    elif s == "Des":
+        return sorted(lst, reverse=True)
+    elif s == "None":
+        return lst
+    else:
+        raise ValueError("Invalid input for sorting order. Use 'Asc', 'Des', or 'None'.")
